8cd6654bcda168ae7c01f09167122e29d0b7d29c
Andrey Egorov <and7643@gmail.com>
2024-10-11 22:04:26 +0300

    Add: alpha mul.

--- src/fixed_point_lib/fixed_point.h
+++ src/fixed_point_lib/fixed_point.h
@@ -6,21 +6,20 @@
 
 #include "meta.h"
 
-
 // Shift has uint8 type because I do not expect it hight then 32
-template<typename value_t, uint8_t Shift> requires std::is_integral_v<value_t>
+template<typename value_t, uint8_t FractionalShift> requires std::is_integral_v<value_t>
   class fixed_point
   {
   private:
     value_t Value;
 
-    // Fraction part mask
+    static constexpr uint8_t Shift = FractionalShift; // For outer usage
     static constexpr value_t Mask = MaskFromShift<value_t, uint8_t, Shift>();
     static constexpr value_t FractionSize = Mask + 1; // Size of fraction part (for casting from float point)
-
+    static constexpr value_t SignBitMask = 1 << (sizeof(value_t) * 8 - 1);
 
     // Constructor from raw value
-    constexpr inline fixed_point( value_t NewValue ) noexcept :
+    explicit constexpr fixed_point( value_t NewValue ) noexcept :
       Value(NewValue) {
     }
 
@@ -38,7 +37,7 @@ template<typename value_t, uint8_t Shift> requires std::is_integral_v<value_t>
         Value(value_t(FloatValue * FractionSize)) {
       }
 
-    // Copy/move constructor
+    // Copy/move constructors
     constexpr fixed_point( const fixed_point & ) = default;
     constexpr fixed_point( fixed_point && ) = default;
     constexpr fixed_point& operator=( const fixed_point & ) = default;
@@ -62,19 +61,62 @@ template<typename value_t, uint8_t Shift> requires std::is_integral_v<value_t>
     
     // Explicit conversion to float point number
     template<typename T> requires std::is_floating_point_v<T>
-      constexpr operator T() {
+      constexpr explicit operator T() {
         return T(Value) / FractionSize;
       }
 
     // Operators
 
-    constexpr inline fixed_point<value_t, Shift> operator+( fixed_point<value_t, Shift> Other ) const noexcept {
-      return fixed_point<value_t, Shift>(Value + Other.Value);
+    // "+"
+
+    constexpr fixed_point operator+( fixed_point Other ) const noexcept {
+      return fixed_point(Value + Other.Value);
     }
     
-    constexpr auto operator-( const fixed_point<value_t, Shift> &Other ) const noexcept {
-      return fixed_point<value_t, Shift>(Value - Other.Value);
+    constexpr fixed_point & operator+=( fixed_point Other ) noexcept {
+      Value += Other.Value;
+      return *this;
     }
+    
+    // "-"
+
+    constexpr fixed_point operator-( fixed_point Other ) const noexcept {
+      return fixed_point(Value - Other.Value);
+    }
+    
+    constexpr fixed_point & operator-=( fixed_point Other ) noexcept {
+      Value -= Other.Value;
+      return *this;
+    }
+    
+    // "*"
+
+    // With int type because this case is simple
+    template<typename other_t> requires std::is_integral_v<other_t>
+      constexpr fixed_point operator*( other_t Other ) const noexcept {
+        return Value * Other;
+      }
+
+    constexpr fixed_point operator*( fixed_point Other ) const noexcept {
+      // Simple way "(Value * Other.Value) >> Shift" can overflow value_t so up to "Shift" higher bits could be lost
+      // Here every component is less then out value so there is no overflow
+      // But now it is slow because(if at debug dissambly is the same as at release) cpp does not store Value and Other.Value at registers and get them from memory several times
+
+      value_t out = Value * (Other.Value >> Shift) + (Value >> Shift) * (Other.Value & Mask) + (((Value & Mask) * (Other.Value & Mask)) >> Shift);
+      if constexpr (std::is_signed_v<value_t>) // Sign check
+        out |= (Value & SignBitMask) ^ (Other.Value & SignBitMask);
+      return out;
+
+      // Too big error rate
+      //return (Value * Other.Value) >> Shift;
+      //return Value * (Other.Value >> Shift) + ((Value * (Other.Value & Mask)) >> Shift);
+    }
+    
+    constexpr fixed_point operator*=( fixed_point Other ) const noexcept {
+      //Value = Value * (Other.Value >> Shift) + (Value >> Shift) * (Other.Value & Mask) + (((Value & Mask) * (Other.Value & Mask)) >> Shift);
+      return *this;
+    }
+    
 
   };
 
--- src/main.cpp
+++ src/main.cpp
@@ -4,31 +4,42 @@
 #include <benchmark/benchmark.h>
 #include <thread>
 
+/**/
 int main( void ) {
-  fixed_point<uint32_t, 16> A{1.9}, B{1.6};
-  double C{double(rand())}, D{double(rand())};
-
-  auto F = A + B;
-  double E = C + D;
+  auto maxD = 1/pow(2, 16);
+  std::printf("\x1b[38;2;0;255;0m!! %.8f !!\x1b[38;2;255;255;255m\n", maxD);
+  for (double x = 0; x < 1000; x += 0.01f) {
+    fixed_point<int32_t, 4> A{x}, B{x};
+    auto d = double(A * B) - double(x * x);
+    if (fabs(d) > 1)
+    //  std::printf("\x1b[38;2;255;0;0m%f - %.8f\x1b[38;2;255;255;255m\n", x, d);
+    //else
+      std::printf("%f - %.8f\n", x, d);
+  }
   return 0;
 }
 
 
 /** /
 
+static volatile float Bunny = 0;
+
 static void FixedPoint(benchmark::State& state) {
   fixed_point<uint32_t, 16> A{rand()};
   fixed_point<uint32_t, 16> B{rand()};
+  fixed_point<uint32_t, 16> C{rand()};
   for (auto _ : state) {
-    A + B;
+     C = A * B;
   }
+  Bunny = float(C);
 }
 
 static void FloatPoint(benchmark::State& state) {
-  double A{double(rand())}, B{double(rand())};
+  double A{double(rand())}, B{double(rand())}, C{double(rand())};
   for (auto _ : state) {
-    A + B + B + B + B + B + B + B + B;
+    C = A * B;
   }
+  Bunny = float(C);
 }
 
 BENCHMARK(FixedPoint);
